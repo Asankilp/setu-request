3365f3420d49b6b1a0e03717353044d4d78cdac9
Asankilp <3384816498@qq.com>
2021-06-26 21:04:47 +0800

    upd

--- nonebot/setu/plugins/setureq.py
+++ nonebot/setu/plugins/setureq.py
@@ -2,18 +2,24 @@ import nonebot
 from nonebot import on_command, CommandSession
 import urllib
 import json
+import sys
+import platform
+
+from requests.sessions import Session, session
 @on_command('setu', aliases=['来一份涩图', '老鸭粉丝汤'], only_to_me=False)
 async def _(session: CommandSession):
     arg = session.current_arg_text.strip().lower()
     if not arg:
         setu_data = await get_setu()
         await session.send(setu_data)
-        await session.send("[CQ:image,file="+dlurl+"]")
-        return
     setu_data = await get_setu(arg)
     await session.send(setu_data)
-    await session.send("[CQ:image,file="+dlurl+"]")
-
+@on_command("ver", only_to_me=False)
+async def _(session: CommandSession):
+    await session.send("setu_qqbot（https://github.com/Asankilp/setu-request）\n本机器人基于NoneBot。涩图API为Lolicon API v1（api.lolicon.app）。\n运行环境：\nPython "+sys.version+"\n操作系统：\n"+platform.platform()+" "+platform.version())
+@on_command("muli", aliases=['目力', '嚎叫'], only_to_me=False)
+async def _(session: CommandSession):
+    await session.send("[CQ:record,file=https://asankilp.github.io/muli.mp3]")
 async def get_setu(arg="") -> str:
     global dlurl
     p1, p2 ,p3 = arg.partition("&")#阻止用户自行添加参数
@@ -30,4 +36,6 @@ async def get_setu(arg="") -> str:
         author = str(data["data"][0]["author"])
         title = str(data["data"][0]["title"])
         tags = str(data["data"][0]["tags"])
-        return "PID:"+pid+" 作者："+author+" 标题："+title+"\n标签："+tags+"\nURL:"+dlurl
+        return "PID:"+pid+" 作者："+author+" 标题："+title+"\n标签："+tags+"\nURL:"+dlurl+"[CQ:image,file="+dlurl+"]"
+    else:
+        return "代码："+str(code)+"\n错误信息："+msg
