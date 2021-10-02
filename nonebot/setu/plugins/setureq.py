f9d409d669ddfdd357a7fb83b6a8cec314bb96a1
Asankilp <3384816498@qq.com>
2021-10-02 22:00:03 +0800

    可以启用或禁用/setu-h命令

--- nonebot/setu/plugins/setureq.py
+++ nonebot/setu/plugins/setureq.py
@@ -4,9 +4,18 @@ import urllib
 import urllib.parse
 import json
 import sys
+import os
 import platform
-
 from requests.sessions import Session, session
+filename = 'setuhdisabled'
+@on_command('togglesetuh', only_to_me=False, permission=perm.SUPERUSER)
+async def _(session: CommandSession):
+    if os.path.exists(filename) == True:
+        os.remove(filename)
+        await session.send("已启用/setu-h。")
+    else:
+        open(filename, mode='w')
+        await session.send("已禁用/setu-h。")
 @on_command('setu', aliases=['来一份涩图', '老鸭粉丝汤'], only_to_me=False)
 async def _(session: CommandSession):
     arg = session.current_arg_text.strip().lower()
@@ -31,22 +40,23 @@ async def _(session: CommandSession):
 @on_command('setu-h', only_to_me=False)
 async def _(session: CommandSession):
     arg = session.current_arg_text.strip().lower()
-    if not arg:
-        await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
-        setu_data = await get_setu_h()
-        if len(setu_data) == 5:
-            await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\nURL:"+setu_data[0]+"\n*不会发送图片。")
+    if os.path.exists(filename) is False:
+        if not arg:
+            await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
+            setu_data = await get_setu_h()
+            if len(setu_data) == 5:
+                await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\nURL:"+setu_data[0]+"\n*不会发送图片。")
+            else:
+                await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
+            del setu_data
         else:
-            await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
-        del setu_data
-    else:
-        await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
-        setu_data = await get_setu_h(arg)
-        if len(setu_data) == 5:
-            await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\nURL:"+setu_data[0]+"\n*不会发送图片。")
-        else:
-            await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
-        del setu_data
+            await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
+            setu_data = await get_setu_h(arg)
+            if len(setu_data) == 5:
+                await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\nURL:"+setu_data[0]+"\n*不会发送图片。")
+            else:
+                await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
+            del setu_data
 @on_command('setub', only_to_me=False)
 async def _(session: CommandSession):
     await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
@@ -60,7 +70,7 @@ async def _(session: CommandSession):
     await session.send("[CQ:record,file=https://asankilp.github.io/muli.mp3]")
 @on_command("help", only_to_me=False)
 async def _(session: CommandSession):
-    await session.send("用法：\n/setu [关键词] 从Lolicon API模糊搜索插画标题，作者，标签的涩图。未提供关键词将随机搜索。\n/setub 从 https://iw233.cn/ 的API（http://iw233.cn/api/Random.php）随机获取涩图。\n/setu-h [关键词] 从Lolicon API模糊搜索插画标题，作者，标签的涩图(R-18)。未提供关键词将随机搜索。\n/ver 查看Bot信息\n/sendmsg <消息> 使此机器人发送指定的消息。仅超级用户可用。")
+    await session.send("用法：\n/setu [关键词] 从Lolicon API模糊搜索插画标题，作者，标签的涩图。未提供关键词将随机搜索。\n/setub 从 https://iw233.cn/ 的API（http://iw233.cn/api/Random.php）随机获取涩图。\n/setu-h [关键词] 从Lolicon API模糊搜索插画标题，作者，标签的涩图(R-18)。未提供关键词将随机搜索。\n/ver 查看Bot信息\n/sendmsg <消息> 使此机器人发送指定的消息。仅超级用户可用。\n/togglesetuh 切换/setu-h命令的可用性（启用/禁用）。对所有会话生效。仅超级用户可用。")
 @on_command("sendmsg", only_to_me=False, permission=perm.SUPERUSER)
 async def _(session: CommandSession):
     arg = session.current_arg_text.strip()
@@ -68,6 +78,10 @@ async def _(session: CommandSession):
         await session.send("未指定消息。发送失败。")
     else:
         await session.send(arg)
+async def togglesetuh():
+    filename = 'setuhdisabled'
+    if os.path.exists(filename) == True:
+        os.remove(filename)
 async def get_setu(arg="") -> str:
     global dlurl
     p1, p2 ,p3 = arg.partition("&")#阻止用户自行添加参数
