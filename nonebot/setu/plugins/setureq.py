0642d053d87fbe10f9e312df0fb35b27872f12d4
Asankilp <3384816498@qq.com>
2021-10-03 22:25:47 +0800

    添加FantasyZone API

--- nonebot/setu/plugins/setureq.py
+++ nonebot/setu/plugins/setureq.py
@@ -5,9 +5,12 @@ import urllib.parse
 import json
 import sys
 import os
+import ssl
 import platform
 from requests.sessions import Session, session
+ssl._create_default_https_context = ssl._create_unverified_context
 filename = 'setuhdisabled'
+#切换lolicon r18
 @on_command('togglesetuh', only_to_me=False, permission=perm.SUPERUSER)
 async def _(session: CommandSession):
     if os.path.exists(filename) == True:
@@ -16,6 +19,7 @@ async def _(session: CommandSession):
     else:
         open(filename, mode='w')
         await session.send("已禁用/setu-h。")
+#lolicon
 @on_command('setu', aliases=['来一份涩图', '老鸭粉丝汤'], only_to_me=False)
 async def _(session: CommandSession):
     arg = session.current_arg_text.strip().lower()
@@ -37,6 +41,7 @@ async def _(session: CommandSession):
         else:
             await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
         del setu_data
+#lolicon r18
 @on_command('setu-h', only_to_me=False)
 async def _(session: CommandSession):
     arg = session.current_arg_text.strip().lower()
@@ -57,11 +62,20 @@ async def _(session: CommandSession):
             else:
                 await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
             del setu_data
+#iw233
 @on_command('setub', only_to_me=False)
 async def _(session: CommandSession):
     await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
-    setu_data = get_setub()
-    await session.send("[CQ:image,file="+setu_data+"]")
+    a = urllib.request.urlopen("http://iw233.cn/api/Random.php")
+    b = str(a.geturl())
+    await session.send("[CQ:image,file="+b+"]")
+#fantasyzone
+@on_command("setuc", only_to_me=False)
+async def _(session: CommandSession):
+    await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
+    data = urllib.request.urlopen("http://api.fantasyzone.cc/tu/?type=url")
+    rediecturl = str(data.geturl())
+    await session.send("[CQ:image,file="+rediecturl+"]")
 @on_command("ver", only_to_me=False)
 async def _(session: CommandSession):
     await session.send("setu_qqbot（https://github.com/Asankilp/setu-request）\n本机器人基于NoneBot。涩图API为Lolicon API v1（api.lolicon.app）。\n运行环境：\nPython "+sys.version+"\n操作系统：\n"+platform.platform()+" "+platform.version())
@@ -70,7 +84,7 @@ async def _(session: CommandSession):
     await session.send("[CQ:record,file=https://asankilp.github.io/muli.mp3]")
 @on_command("help", only_to_me=False)
 async def _(session: CommandSession):
-    await session.send("用法：\n/setu [关键词] 从Lolicon API模糊搜索插画标题，作者，标签的涩图。未提供关键词将随机搜索。\n/setub 从 https://iw233.cn/ 的API（http://iw233.cn/api/Random.php）随机获取涩图。\n/setu-h [关键词] 从Lolicon API模糊搜索插画标题，作者，标签的涩图(R-18)。未提供关键词将随机搜索。\n/ver 查看Bot信息\n/sendmsg <消息> 使此机器人发送指定的消息。仅超级用户可用。\n/togglesetuh 切换/setu-h命令的可用性（启用/禁用）。对所有会话生效。仅超级用户可用。")
+    await session.send("用法：\n/setu [关键词] 从Lolicon API模糊搜索插画标题，作者，标签的涩图。未提供关键词将随机搜索。\n/setub 从 https://iw233.cn/ 的API（http://iw233.cn/api/Random.php）随机获取涩图。\n/setuc 从FantasyZone API（https://api.fantasyzone.cc/#/tu）随机获取涩图。\n/setu-h [关键词] 从Lolicon API模糊搜索插画标题，作者，标签的涩图(R-18)。未提供关键词将随机搜索。\n/ver 查看Bot信息\n/sendmsg <消息> 使此机器人发送指定的消息。仅超级用户可用。\n/togglesetuh 切换/setu-h命令的可用性（启用/禁用）。对所有会话生效。仅超级用户可用。")
 @on_command("sendmsg", only_to_me=False, permission=perm.SUPERUSER)
 async def _(session: CommandSession):
     arg = session.current_arg_text.strip()
@@ -82,6 +96,7 @@ async def togglesetuh():
     filename = 'setuhdisabled'
     if os.path.exists(filename) == True:
         os.remove(filename)
+#lolicon
 async def get_setu(arg="") -> str:
     global dlurl
     p1, p2 ,p3 = arg.partition("&")#阻止用户自行添加参数
@@ -101,6 +116,7 @@ async def get_setu(arg="") -> str:
         return [dlurl, pid, author, title, tags]
     else:
         return [str(code), msg]
+#lolicon r18
 async def get_setu_h(arg="") -> str:
     global dlurl
     p1, p2 ,p3 = arg.partition("&")#阻止用户自行添加参数
@@ -120,7 +136,3 @@ async def get_setu_h(arg="") -> str:
         return [dlurl, pid, author, title, tags]
     else:
         return [str(code), msg]
-def get_setub():
-    a = urllib.request.urlopen("http://iw233.cn/api/Random.php")
-    b = str(a.geturl())
-    return b
