c53465d3895e21ac3f4fb658f7c83c12991f7847
Asankilp <3384816498@qq.com>
2021-10-04 20:49:02 +0800

    添加关键词与标签屏蔽列表，新增config.json

--- nonebot/setu/plugins/setureq.py
+++ nonebot/setu/plugins/setureq.py
@@ -6,10 +6,31 @@ import json
 import sys
 import os
 import ssl
+import re
 import platform
 from requests.sessions import Session, session
 ssl._create_default_https_context = ssl._create_unverified_context
 filename = 'setuhdisabled'
+setu_bannedkeywords = []
+setu_bannedtags = []
+setu_h_bannedkeywords = []
+setu_h_bannedtags = []
+config = ["{\"setu\":{\"banned_keywords\":[],\"banned_tags\":[]},\"setu-h\":{\"banned_keywords\":[],\"banned_tags\":[]}}"]
+#读取配置文件
+if os.path.exists("config.json") == False:
+    print("config.json not found. Creating...")
+    with open("config.json", mode="w") as newconf:
+        newconf.writelines(config)
+try:
+    with open("config.json", encoding="utf-8") as conf:
+        a = json.load(conf)
+        setu_bannedkeywords = a['setu']['banned_keywords']
+        setu_bannedtags = a['setu']['banned_tags']
+        setu_h_bannedkeywords = a['setu-h']['banned_keywords']
+        setu_h_bannedtags = a['setu-h']['banned_tags']
+except:
+    print("Unable to read config.json.")
+    pass
 #切换lolicon r18
 @on_command('togglesetuh', only_to_me=False, permission=perm.SUPERUSER)
 async def _(session: CommandSession):
@@ -22,46 +43,43 @@ async def _(session: CommandSession):
 #lolicon
 @on_command('setu', aliases=['来一份涩图', '老鸭粉丝汤'], only_to_me=False)
 async def _(session: CommandSession):
-    arg = session.current_arg_text.strip().lower()
-    if not arg:
-        await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
-        setu_data = await get_setu()
-        if len(setu_data) == 5:
-            await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\n标签："+setu_data[4]+"\nURL:"+setu_data[0])
-            await session.send("[CQ:image,file="+setu_data[0]+"]")
-        else:
-            await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
-        del setu_data
+    arg = session.current_arg_text.strip()
+    for i in setu_bannedkeywords:
+        if re.search(i, arg) is not None:
+            await session.send("此关键词已被屏蔽。")
+            return
+    await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
+    setu_data = await get_setu(arg)
+    if len(setu_data) == 5:
+        for tag in setu_data[4]:
+            if tag in setu_bannedtags:
+                await session.send("API返回的涩图的其中一个或多个标签已被屏蔽。")
+                return
+        await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\n标签："+str(setu_data[4])+"\nURL:"+setu_data[0])
+        await session.send("[CQ:image,file="+setu_data[0]+"]")
     else:
-        await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
-        setu_data = await get_setu(arg)
-        if len(setu_data) == 5:
-            await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\n标签："+setu_data[4]+"\nURL:"+setu_data[0])
-            await session.send("[CQ:image,file="+setu_data[0]+"]")
-        else:
-            await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
-        del setu_data
+        await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
+    del setu_data
 #lolicon r18
 @on_command('setu-h', only_to_me=False)
 async def _(session: CommandSession):
-    arg = session.current_arg_text.strip().lower()
+    arg = session.current_arg_text.strip()
     if os.path.exists(filename) is False:
-        if not arg:
-            await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
-            setu_data = await get_setu_h()
-            if len(setu_data) == 5:
-                await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\nURL:"+setu_data[0]+"\n*不会发送图片。")
-            else:
-                await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
-            del setu_data
+        for i in setu_h_bannedkeywords:
+            if re.search(i, arg) is not None:
+                await session.send("此关键词已被屏蔽。")
+            return
+        await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
+        setu_data = await get_setu_h(arg)
+        if len(setu_data) == 5:
+            for tag in setu_data[4]:
+                if tag in setu_h_bannedtags:
+                    await session.send("API返回的涩图的其中一个或多个标签已被屏蔽。")
+                    return
+            await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\nURL:"+setu_data[0]+"\n*不会发送图片。")
         else:
-            await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
-            setu_data = await get_setu_h(arg)
-            if len(setu_data) == 5:
-                await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\nURL:"+setu_data[0]+"\n*不会发送图片。")
-            else:
-                await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
-            del setu_data
+            await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
+        del setu_data
 #iw233
 @on_command('setub', only_to_me=False)
 async def _(session: CommandSession):
@@ -112,7 +130,7 @@ async def get_setu(arg="") -> str:
         pid = str(data["data"][0]["pid"])
         author = str(data["data"][0]["author"])
         title = str(data["data"][0]["title"])
-        tags = str(data["data"][0]["tags"])
+        tags = list(data["data"][0]["tags"])
         return [dlurl, pid, author, title, tags]
     else:
         return [str(code), msg]
