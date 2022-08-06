546aebcf802e0e4428e41145dceefc58ca7b679d
Asankilp <3384816498@qq.com>
2022-08-06 22:54:53 +0800

    移除屏蔽功能+优化json读取+涩涩控制

--- bot_plugins/apis/apis.py
+++ bot_plugins/apis/apis.py
@@ -1,8 +1,9 @@
 import os
 import json
-import urllib
 import urllib.parse
 import urllib.request
+
+from seturequest import get_setu_from_loliconv1
 async def togglesetuh():
     filename = 'setuhdisabled'
     if os.path.exists(filename) == True:
@@ -13,11 +14,7 @@ bakalist = [int, float, str, bool, dict, list, type(None)]
 async def get_setu(arg: str) -> str:
     global dlurl
     p1, p2, p3 = arg.partition("&")  # 阻止用户自行添加参数
-    word = urllib.parse.quote(p1)
-    ree = urllib.request.urlopen(
-        'https://api.lolicon.app/setu/v1/?num=1&size1200=true&proxy=i.pixiv.re&keyword='+word)  # 从api获取json
-    de = ree.read().decode()  # 解码
-    data = json.loads(de)
+    data = get_setu_from_loliconv1(p1)
     code = int(data["code"])
     msg = str(data["msg"])
     if code == 0:
@@ -36,11 +33,7 @@ async def get_setu(arg: str) -> str:
 async def get_setu_h(arg: str) -> str:
     global dlurl
     p1, p2, p3 = arg.partition("&")  # 阻止用户自行添加参数
-    word = urllib.parse.quote(p1)
-    ree = urllib.request.urlopen(
-        'https://api.lolicon.app/setu/v1/?num=1&size1200=true&proxy=i.pixiv.re&r18=1&keyword='+word)  # 从api获取json
-    de = ree.read().decode()  # 解码
-    data = json.loads(de)
+    data = get_setu_from_loliconv1(p1, r18=1)
     code = int(data["code"])
     msg = str(data["msg"])
     if code == 0:
@@ -63,21 +56,22 @@ async def url_log(url: str, logfilename: str) -> str:
         logfile.write(url+"\n")
         logfile.close()
 
-async def getconfig(key: str, default=bakalist):
+def getconfig(key: str = None, default=bakalist):
     with open("setubot_config.json", mode="r", encoding="utf-8") as config:
         data = json.loads(config.read())
         if key is None:
             return data
         else:
             return data.get(str(key), default)
-async def setconfig(key: str, value: bakalist):
+def setconfig(key: str, value: bakalist):
     if type(value) not in bakalist:
-         return False
+         print("Aaaaaaaaa")
     if key is None:
         data = value
     else:
         data = getconfig(default=dict())
         data[str(key)] = value
+        print(data)
     try:
         with open("setubot_config.json", mode="w", encoding="utf-8") as config:
             text = json.dumps(data, indent=4, ensure_ascii=False)
--- bot_plugins/setureq.py
+++ bot_plugins/setureq.py
@@ -7,30 +7,21 @@ import traceback
 import platform
 import datetime
 import urllib.parse
+from anyio import run_async_from_thread
+from requests import JSONDecodeError
 
 from bot_plugins.apis.apis import *
 from nonebot import on_command, CommandSession, permission as perm
 import nonebot.log as log
-filename = 'setuhdisabled'
-setu_bannedkeywords = []
-setu_bannedtags = []
-setu_h_bannedkeywords = []
-setu_h_bannedtags = []
-setu_h_enable = False
-enable_url_logging = False
+def setues_is_enable(sender: perm.SenderRoles):
+    if getconfig("setues_enable") is True:
+        return True
+    else:
+        return False
 config = {
-    "setu":{
-        "banned_keywords":[],
-        "banned_tags":[]
-    },
-    "setu-h":{
-        "banned_keywords":[],
-        "banned_tags":[],
-        "enable": False
-    },
-    "global":{
-        "enable_url_logging": False
-    }
+    "setues_enable": True,
+    "setu_h_enable": False,
+    "enable_url_logging": False
 }
 clockstart = datetime.datetime.now()
 log.logger.debug("机器人运行时间："+str(clockstart))
@@ -39,56 +30,52 @@ setu_counter = 0
 setub_counter = 0
 setuc_counter = 0
 setu_h_counter = 0
+def force_create_config():
+    with open("setubot_config.json", mode="w") as newconf:
+        newconf.writelines(json.dumps(config, indent=4))
 # 读取配置文件
 if not os.path.exists("setubot_config.json"):
-    log.logger.debug("setubot_config.json not found. Creating...")
-    with open("setubot_config.json", mode="w") as newconf:
-        newconf.writelines(json.dumps(config))
+    log.logger.debug("setubot_config.json不存在。 正在创建...")
+    force_create_config()
 try:
-    with open("setubot_config.json", encoding="utf-8") as conf:
-        a = json.load(conf)
-        setu_bannedkeywords = a['setu']['banned_keywords']
-        setu_bannedtags = a['setu']['banned_tags']
-        setu_h_bannedkeywords = a['setu-h']['banned_keywords']
-        setu_h_bannedtags = a['setu-h']['banned_tags']
-        setu_h_enable = a['setu-h']['enable']
-        enable_url_logging = a['global']['enable_url_logging']
-        
-except:
-    log.logger.debug("Unable to read setubot_config.json.")
-    traceback.print_exc()
-    pass
-# 切换lolicon r18
+    for key in config:
+        if json.loads(open("setubot_config.json", encoding="utf-8").read()).get(key) is None:
+            result = setconfig(key=key, value=config[key])
+            log.logger.debug("发现配置文件未写入的键值对，已自动写入："+key+": "+str(config[key]))
+            log.logger.debug(str(result))
+except json.decoder.JSONDecodeError:
+    log.logger.debug("setubot_config.json 读取失败，已自动重置")
+    force_create_config()
+    sys.exit(-1)
+# 切换涩图命令
 
 
-@on_command('togglesetuh', only_to_me=False, permission=perm.SUPERUSER)
+@on_command('togglesetues', only_to_me=False, permission=perm.SUPERUSER)
 async def _(session: CommandSession):
-    if os.path.exists(filename):
-        os.remove(filename)
-        await session.send("已启用/setu-h。")
-    else:
-        open(filename, mode='w')
-        await session.send("已禁用/setu-h。")
+    if getconfig("setues_enable") is True:
+        setconfig(key="setues_enable", value=False)
+        await session.send("不可以涩涩")
+    elif getconfig("setues_enable") is False:
+        setconfig(key="setues_enable", value=True)
+        await session.send("可以涩涩")
+# 切换lolicon r18
+@on_command("togglesetuh", only_to_me=False, permission=perm.SUPERUSER)
+async def _(session: CommandSession):
+    if getconfig("setu_h_enable") is True:
+        setconfig(key="setu_h_enable", value=False)
+        await session.send("不可以色色")
+    elif getconfig("setu_h_enable") is False:
+        setconfig(key="setu_h_enable", value=True)
+        await session.send("可以色色")
 # lolicon
-
-
-@on_command('setu', aliases=['来一份涩图', '老鸭粉丝汤'], only_to_me=False)
+@on_command('setu', aliases=['来一份涩图', '老鸭粉丝汤'], only_to_me=False, permission=setues_is_enable)
 async def _(session: CommandSession):
     global setu_counter
     arg = session.current_arg_text.strip()
-    for i in setu_bannedkeywords:
-        if re.search(i, arg) is not None:
-            await session.send("此关键词已被屏蔽。")
-            return
     await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
     setu_data = await get_setu(arg)
     if len(setu_data) == 5:
-        for tag in setu_data[4]:
-            if tag in setu_bannedtags:
-                await session.send("API返回的涩图的其中一个或多个标签已被屏蔽。")
-                return
-        if enable_url_logging == True:
-            await url_log(setu_data[0], logfilename=logfilename)
+        if getconfig("enable_url_logging") == True: await url_log(setu_data[0], logfilename=logfilename)
         await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\n标签："+str(setu_data[4])+"\nURL:"+setu_data[0])
         await session.send("[CQ:image,file="+setu_data[0]+"]")
         setu_counter = setu_counter + 1
@@ -102,11 +89,7 @@ async def _(session: CommandSession):
 async def _(session: CommandSession):
     global setu_h_counter
     arg = session.current_arg_text.strip()
-    if os.path.exists(filename) is False:
-        for i in setu_h_bannedkeywords:
-            if re.search(i, arg) is not None:
-                await session.send("此关键词已被屏蔽。")
-                return
+    if getconfig("setu_h_enable") is True: #setuh_enable
         await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
         setu_data = await get_setu_h(arg)
         if len(setu_data) == 5:
@@ -114,37 +97,36 @@ async def _(session: CommandSession):
                 if tag in setu_h_bannedtags:
                     await session.send("API返回的涩图的其中一个或多个标签已被屏蔽。")
                     return
-            if enable_url_logging == True:
-                await url_log(setu_data[0], logfilename=logfilename)
+            if getconfig("enable_url_logging") == True: await url_log(setu_data[0], logfilename=logfilename)
             await session.send("PID:"+setu_data[1]+" 作者："+setu_data[2]+" 标题："+setu_data[3]+"\nURL:"+setu_data[0]+"\n*不会发送图片。")
             setu_h_counter = setu_h_counter + 1
         else:
             await session.send("代码："+setu_data[0]+"\n错误信息："+setu_data[1])
         del setu_data
+    else:
+        await session.send("不可以色色！")
 # iw233
 
 
-@on_command('setub', only_to_me=False)
+@on_command('setub', only_to_me=False, permission=setues_is_enable)
 async def _(session: CommandSession):
     global setub_counter
     await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
     a = urllib.request.urlopen("http://iw233.cn/api/Random.php")
     b = str(a.geturl())
-    if enable_url_logging == True:
-        await url_log(b, logfilename=logfilename)
+    if getconfig("enable_url_logging") == True: await url_log(b, logfilename=logfilename)
     await session.send("[CQ:image,file="+b+"]")
     setub_counter = setub_counter + 1
 # fantasyzone
 
 
-@on_command("setuc", only_to_me=False)
+@on_command("setuc", only_to_me=False, permission=setues_is_enable)
 async def _(session: CommandSession):
     global setuc_counter
     await session.send("搜索涩图中。请耐心等待。\n一段时间后仍未响应，请重试或联系Bot管理员。")
     data = urllib.request.urlopen("http://api.fantasyzone.cc/tu/?type=url")
     rediecturl = str(data.geturl())
-    if enable_url_logging == True:
-        await url_log(rediecturl, logfilename=logfilename)
+    if getconfig("enable_url_logging") == True: await url_log(rediecturl, logfilename=logfilename)
     await session.send("[CQ:image,file="+rediecturl+"]")
     setuc_counter = setuc_counter + 1
 
@@ -179,7 +161,9 @@ async def _(session: CommandSession):
     /setu-h [关键词] 从Lolicon API模糊搜索插画标题，作者，标签的涩图(R-18)。未提供关键词将随机搜索。
     /ver 查看Bot信息
     /sendmsg <消息> 使此机器人发送指定的消息。仅超级用户可用。
-    /togglesetuh 切换/setu-h命令的可用性（启用/禁用）。对所有会话生效。仅超级用户可用。""")
+    /togglesetuh 切换/setu-h命令的可用性（启用/禁用）。对所有会话生效。仅超级用户可用。
+    /togglesetues 切换除/setu-h外所有请求涩图命令的可用性（启用/禁用）。对所有会话生效。仅超级用户可用。
+    /getconfig <key> 获取在setubot_config.json中指定键的值。仅超级用户可用。""")
 
 
 @on_command("sendmsg", only_to_me=False, permission=perm.SUPERUSER)
@@ -193,5 +177,5 @@ async def _(session: CommandSession):
 @on_command("getconfig", permission=perm.SUPERUSER, only_to_me=False)
 async def _(session: CommandSession):
     arg = session.current_arg_text.strip()
-    result = await getconfig(arg)
+    result = getconfig(arg)
     await session.send(str(result))
